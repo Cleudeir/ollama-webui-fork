d52b6778b9d0803c48ce3ef6bbb864f2c556c73d
Cleudeir <Cleudeirsilva@gmail.com>
2024-04-22 03:27:21 +0000

    up

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
-export const OLLAMA_API_BASE_URL = `http://localhost:11434/api`;
+export const OLLAMA_API_BASE_URL = `https://ai.eixoxprojetos.com.br/api`;
 export const WEB_UI_VERSION = "v0.0.1-lite";
+export const defaultModel = 'mistral:7b-text-v0.2-q2_K'
